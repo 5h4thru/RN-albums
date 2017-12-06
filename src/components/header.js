ed2683cf033f0f7f73e3d4796a0633c6641f12ca
Palagummi <paladurg@amazon.com>
2017-12-05 18:06:13 -0800

    Chapter 'HTTP Request with React Native ' Checkpoint * Implement axios library * Component Level States and how to use them * Bonus: Scaffold for AlbumDetail

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { Text, View } from 'react-native';
 
 // Create a component
-const Header = (props) => {
+const Header = props => {
   const { textStyle, viewStyle } = styles;
   return (
     <View style={viewStyle}>
@@ -23,7 +23,7 @@ const styles = {
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.2,
     elevation: 2,
-    position: 'relative',
+    position: 'relative'
   },
   textStyle: {
     fontSize: 20
